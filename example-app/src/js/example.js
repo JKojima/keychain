3abc2542a16f4edecc6dde37ef01da64bcf4cebb
kenzykozyma <kojima.kenji@amsinc.co.jp>
2025-05-14 10:35:37 +0900

    Add DotbitjpKeychain podspec and update version to 0.0.5 in package.json; remove example app files

--- example-app/src/js/example.js
+++
@@ -1,6 +0,0 @@
-import { Keychain } from '@dotbit/keychain';
-
-window.testEcho = () => {
-    const inputValue = document.getElementById("echoInput").value;
-    Keychain.echo({ value: inputValue })
-}
